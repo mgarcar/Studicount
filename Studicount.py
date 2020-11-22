087fbba576c49c1938695899dbf18d242f2be885
mgarcar <74672334+mgarcar@users.noreply.github.com>
2020-11-22 19:24:07 +0100

    Add files via upload

---
+++ Studicount.py
@@ -0,0 +1,217 @@
+discount = {}
+discount["D1"] = (0, "Restaurants", "50% Discount in WELKHOMEclub")
+discount["D2"] = (2, "Restaurants", "15% Discount in UberEats first order")
+discount["D3"] = (0, "Restaurants", "25% Discounts Club Vips")
+discount["D4"] = (0, "Restaurants", "15% Discount in Cafeteria Sevi")
+discount["D5"] = (0, "Restaurants", "61% Discount in Paraiso del Desierto")
+discount["D6"] = (2, "Restaurants", "2 menus for 7€ in BurgerKing ")
+discount["D7"] = (0, "Restaurants", "20% Discount in Oceanika madrid")
+discount["D8"] = (0, "Restaurants", "20% Discount in Rahduni Indian")
+discount["D9"] = (0, "Restaurants", "30% Discount in al son de Cuba")
+discount["D10"] = (0, "Restaurants", "15% Discount in Mamma Kebab")
+discount["D11"] = (2, "Restaurants", "2x1 Sandy in Burger King")
+discount["D12"] = (2, "Restaurants", "2x1 mini mcflurry in McDonalds")
+discount["D13"] = (0, "Restaurants", "30% Discount in Restaurante de Sichuan")
+discount["D14"] = (0, "Restaurants", " 1 free coffee with the purchase of a breakfast in cafeteria caracas")
+discount["D15"] = (0, "Restaurants", "30% Discount in Maison Melie")
+discount["D16"] = (0, "Restaurants", "15% Discount in el asador de pollos")
+discount["D17"] = (2, "Restaurants", "71% Discount in papa john pizza through uber eats")
+discount["D18"] = (0, "Restaurants", "25% Discount in Uber eats with an oder higher than 25€")
+discount["D19"] = (2, "Restaurants", "BigMac for 1€ in McDonalds")
+discount["D20"] = (0, "Restaurants", "25% Discount in KFC through Uber Eats")
+discount["D21"] = (1, "Restaurants", "50% Discount in taberna Lopez")
+discount["D22"] = (1, "Restaurants", "30% Discount in restaurante el parador Segovia")
+discount["D23"] = (1, "Restaurants", "25% Discount in Meson don Sancho")
+discount["D24"] = (1, "Restaurants", "50% Discount in Oso Blanco")
+discount["D25"] = (2, "Restaurants", "todo a 1€ Wednesday and Sunday in 100Montaditos")
+discount["D26"]= (2, "Fashion", "40% Discount in Converse")
+discount["D27"]= (2, "Fashion", "20% Discount in Nike")
+discount["D28"]= (2, "Fashion", "10% Discounts in Under Armour")
+discount["D29"]= (2, "Fashion", "30% Discount in Adidas")
+discount["D30"]= (2, "Fashion", "20% Discount in New Balance")
+discount["D31"]= (2, "Fashion", "15% Discount in Shein")
+discount["D32"]= (2, "Fashion", "35% Discount in Reebok")
+discount["D33"]= (2, "Fashion", "10% Discount in Tommmy Hilfiger")
+discount["D34"]= (2, "Fashion", "15% Discount in QuickSilver")
+discount["D35"]= (2, "Fashion", "15% Discount in The Kooples Paris")
+discount["D36"]= (2, "Fashion", "10% Discount in ASOS")
+discount["D37"]= (2, "Fashion", "20% Discount in Jack & Jones")
+discount["D38"]= (2, "Fashion", "15% Discount in ROXY")
+discount["D39"]= (2, "Fashion", "15% Discount in MISSGUIDED")
+discount["D40"]= (2, "Fashion", "10% Discount in DR.Martens")
+discount["D41"]= (2, "Fashion", "10% Discount in COACH")
+discount["D42"]= (2, "Fashion", "10% Discount in Levis")
+discount["D43"]= (2, "Fashion", "10% Discount in Calvin Klein")
+discount["D44"]= (2, "Fashion", "10% Discount in Harvey Nichols")
+discount["D45"]= (2, "Fashion", "30% Discount in ALLSAINTS")
+discount["D46"]= (2, "Fashion", "10% Discount in SIZE?")
+discount["D47"]= (2, "Fashion", "50% Discount in Boohoo")
+discount["D48"]= (2, "Fashion", "20% Discount in Lovehoney")
+discount["D49"]= (2, "Fashion", "20% Discount in Ray Ban Accessories")
+discount["D50"]= (2, "Fashion", "15%Discount in Spartoo")
+discount["D51"]= (2, "Groceries", "40% Discount in Carrefour potatoes")
+discount["D52"]= (0, "Groceries", "25% Discount in Alcampo sugar")
+discount["D53"]= (0, "Groceries", "20% Discount in ElCorteIngles apples")
+discount["D54"]= (1, "Groceries", "2X1 Discount in Mercadona, buy one chocolate cake and get another for free!")
+discount["D55"]= (2, "Groceries", "10% Discount in Dia oranges")
+discount["D56"]= (2, "Groceries", "20% Discount in Lidl bananas")
+discount["D57"]= (0, "Groceries", "30% Discount in SuperSol lasagna")
+discount["D58"]= (1, "Groceries", "10% Discount in Carrefour Express chocolate")
+discount["D59"]= (1, "Groceries", "25% Discount in Aldi juice")
+discount["D60"]= (2, "Groceries", "50% Discount in ElCorteIngles pasta")
+discount["D61"]= (1, "Groceries", "60% Discount in Dia tomatoes")
+discount["D62"]= (1, "Groceries", "10% Discount in Mercadona salad")
+discount["D63"]= (1, "Groceries", "2X1 Discount in Aldi cheese")
+discount["D64"]= (0, "Groceries", "25% Discount in Mercadona cereals")
+discount["D65"]= (0, "Groceries", "35% Discount in Dia cereals")
+discount["D66"]= (0, "Groceries", "15% Discount in Mercadona cereals")
+discount["D67"]= (0, "Groceries", "20% Discount in LaDespensa flour")
+discount["D68"]= (0, "Groceries", "13% Discount in Mercadona cheesecake")
+discount["D69"]= (0, "Groceries", "40% Discount in Dia nuts")
+discount["D70"]= (0, "Groceries", "20% Discount in Mercadona cereals")
+discount["D71"]= (0, "Groceries", "14% Discount in Dia cocos")
+discount["D72"]= (0, "Groceries", "20% Discount in LaDespensa supermarket Fish")
+discount["D73"]= (0, "Groceries", "10% Discount in Aldi cereals")
+discount["D74"]= (0, "Groceries", "13% Discount in Aldi milk")
+discount["D75"]= (0, "Groceries", "50% Discount in Aldi hamburger meat")
+discount["D76"]= (0, "Fun", "10% discount in the Retiro Canoes")
+discount["D77"]= (0, "Fun", "15% discount in VIP Beyonce")
+discount["D78"]= (0, "Fun", "20% discount in  festival")
+discount["D79"]= (1, "Fun", "15% discount in Plaza concert")
+discount["D80"]= (1, "Fun", "5% discount in Joker movie")
+discount["D81"]= (0, "Fun", "15% discount in Holy Run")
+discount["D82"]= (1, "Fun", "10% discount in Dreamfit membership for students")
+discount["D83"]= (2, "Fun", "10% discount in Dunking Donuts")
+discount["D84"]= (0, "Fun", "20% discount in Sumba lessons")
+discount["D85"]= (2, "Fun", "10% discount in online video game")
+discount["D86"]= (0, "Fun", "50% discount for college students going to Nuit Club")
+discount["D87"]= (1, "Fun", "20% discount in Irish drinks for IE students")
+discount["D88"]= (0, "Fun", "15% discount in cooking courses")
+discount["D89"]= (1, "Fun", "10% discount in Driving lessons at Santa Columba ")
+discount["D90"]= (0, "Fun", "20% discount for Dvicio concert last minute tickets")
+discount["D91"]= (0, "Fun", "50% discount for drinks at Tiffanys")
+discount["D92"]= (0, "Fun", "15% discount for Siclo classes on Sundays")
+discount["D93"]= (0, "Fun", "50% discount in madrid Fashion week")
+discount["D94"]= (0, "Fun", "15% in Ed Sheeran concert")
+discount["D95"]= (0, "Fun", "20% discount for drinks in Toros")
+discount["D96"]= (2, "Fun", "50% discount in all Cien Montaditos Beers")
+discount["D97"]= (0, "Fun", "10% discount in Toros tickets")
+discount["D98"]= (0, "Fun", "30% discount in tickets for Hipodromo de la Zarzuela")
+discount["D99"]= (0, "Fun", "30% discount in Flamenco concert")
+discount["D100"]= (0,"Fun","10% discount for Real Madrid vs. Atletico football ticket")
+
+def search(discount):
+    while True:
+        category = input("Choose from the following categories: \n - Restaurants\n - Fashion\n - Groceries\n - Fun\n ")
+        location = input("please choose one location: \n - Madrid\n - Segovia\n - Both\n: ")
+        filtered_discounts = {}
+
+        if category == "Restaurants" and location == "Madrid":
+            for key, value in discount.items():
+                if value[1] == "Restaurants":
+                        if value[0] == 0 or value[0] == 2:
+                            filtered_discounts[key] = value
+            print("\n".join("{}\t{}".format(k, v) for k, v in filtered_discounts.items()))
+            break
+
+        if category == "Restaurants" and location == "Segovia":
+           for key, value in discount.items():
+                if value[1] == "Restaurants":
+                    if value[0] == 1 or value[0] == 2:
+                        filtered_discounts[key] = value
+           print("\n".join("{}\t{}".format(k, v) for k, v in filtered_discounts.items()))
+           break
+
+        if category == "Restaurants" and location == "Both":
+           for key, value in discount.items():
+               if  value[1] == "Restaurants":
+                    filtered_discounts[key] = value
+           print("\n".join("{}\t{}".format(k, v) for k, v in filtered_discounts.items()))
+           break
+
+        if category == "Fashion" and location == "Madrid":
+           for key, value in discount.items():
+               if value[1] == "Fashion":
+                   if value[0] == 0 or value[0] == 2:
+                       filtered_discounts[key] = value
+           print("\n".join("{}\t{}".format(k, v) for k, v in filtered_discounts.items()))
+           break
+
+        if category == "Fashion" and location == "Segovia":
+           for key, value in discount.items():
+              if value[1] == "Fashion":
+                  if value[0] == 1 or value[0] == 2:
+                      filtered_discounts[key] = value
+           print("\n".join("{}\t{}".format(k, v) for k, v in filtered_discounts.items()))
+           break
+
+        if category == "Fashion" and location == "Both":
+           for key, value in discount.items():
+              if value[1] == "Fashion":
+                   filtered_discounts[key] = value
+           print("\n".join("{}\t{}".format(k, v) for k, v in filtered_discounts.items()))
+           break
+
+        if category == "Groceries" and location == "Madrid":
+           for key, value in discount.items():
+              if value[1] == "Groceries":
+                  if value[0] == 0 or value[0] == 2:
+                      filtered_discounts[key] = value
+           print("\n".join("{}\t{}".format(k, v) for k, v in filtered_discounts.items()))
+           break
+
+        if category == "Groceries" and location == "Segovia":
+            for key, value in discount.items():
+               if value[1] == "Groceries":
+                   if value[0] == 1 or value[0] == 2:
+                       filtered_discounts[key] = value
+            print("\n".join("{}\t{}".format(k, v) for k, v in filtered_discounts.items()))
+            break
+
+        if category == "Groceries" and location == "Both":
+             for key, value in discount.items():
+                if value[1] == "Groceries":
+                   filtered_discounts[key] = value
+             print("\n".join("{}\t{}".format(k, v) for k, v in filtered_discounts.items()))
+             break
+
+        if category == "Fun" and location == "Madrid":
+             for key, value in discount.items():
+                if value[1] == "Fun":
+                    if value[0] == 0 or value[0] == 2:
+                        filtered_discounts[key] = value
+             print("\n".join("{}\t{}".format(k, v) for k, v in filtered_discounts.items()))
+             break
+
+        if category == "Fun" and location == "Segovia":
+             for key, value in discount.items():
+                if value[1] == "Fun":
+                    if value[0] == 1 or value[0] == 2:
+                        filtered_discounts[key] = value
+             print("\n".join("{}\t{}".format(k, v) for k, v in filtered_discounts.items()))
+             break
+
+        if category == "Fun" and location == "Both":
+             for key, value in discount.items():
+                 if value[1] == "Fun":
+                    filtered_discounts[key] = value
+             print("\n".join("{}\t{}".format(k, v) for k, v in filtered_discounts.items()))
+             break
+
+        else:
+            print("Not a valid option. Please enter the category and the location again")  # warning of invalid imput, menu will diplay again
+
+search(discount)
+
+def folder(discount):
+    name = input("please enter the name of your folder: ")
+    name = {}
+    selected_dis = input("please enter the name of the discount you would like to include in your folder (example D22) (press enter when you have finished): ")
+    while selected_dis != "":
+        name[selected_dis] = discount[selected_dis]
+        selected_dis = input("please enter the name of the discount you would like to include in your folder (example D22) (press enter when you have finished): ")
+
+
+    print("\n".join("{}\t{}".format(k, v) for k, v in name.items()))
+
+folder(discount)
